2f7d68c90cdfcc6f05988e5a461e6443af8ddb88
Shaidyk <61831338+Shaidyk@users.noreply.github.com>
2020-07-22 15:07:00 +0300

    migrate and admin

--- personal/models.py
+++ personal/models.py
@@ -3,32 +3,38 @@ import datetime
 
 
 class Client(models.Model):
+    """Клиент"""
     first_name = models.CharField(max_length=100)
     phone = models.PositiveIntegerField()
 
 
 class Region(models.Model):
+    """Район"""
     name = models.CharField(max_length=200)
 
 
 class Restaurant(models.Model):
+    """Ресторан"""
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=200)
     region = models.ForeignKey(Region, on_delete=models.CASCADE)
 
 
 class Courier(models.Model):
+    """Курьер"""
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     region = models.ForeignKey(Region, on_delete=models.CASCADE)
 
 
 class MenuItem(models.Model):
+    """Позиция в меню"""
     name = models.CharField(max_length=150)
     price = models.TextField(max_length=1000)
 
 
 class Order(models.Model):
+    """Закакз"""
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     courier = models.ForeignKey(Courier, on_delete=models.CASCADE)
@@ -38,5 +44,6 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
+    """Позиция меню в заказе"""
     menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
