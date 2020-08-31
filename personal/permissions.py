4c3ddfbec1a3a73a2bf6b60048a8e13c54b70577
Shaidyk <61831338+Shaidyk@users.noreply.github.com>
2020-08-31 15:10:53 +0300

    create groups

--- personal/permissions.py
+++ personal/permissions.py
@@ -3,9 +3,9 @@ from rest_framework.permissions import BasePermission
 
 class CanConfirmOrder(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_staff
+        return request.user.is_Operators
 
 
 class CanCompleteOrder(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_staff
+        return request.user.is_Couriers
